dee1f8cb70e9e811c6eaa83d1219816bfd1b1be8
Mathew Hany <mathew.hanybb@gmail.com>
2025-04-30 17:02:47 +0300

    feat: add notification strategy structure

---
+++ notification-service/src/main/java/org/example/notificationservice/models/NotificationStrategyType.java
@@ -0,0 +1,6 @@
+package org.example.notificationservice.models;
+
+public class NotificationStrategyType {
+    public static final String INBOX = "inbox"; // Adds the notification to the database
+    public static final String EMAIL = "email";
+}
