3aeffc3d1d4c68900af5c73302b7b88564ffb8a1
SaraAlajmy <saraalagami@gmail.com>
2025-05-01 13:05:36 +0300

    add token cache

---
+++ gateway/src/main/java/com/example/gateway/config/RedisCacheConfig.java
@@ -0,0 +1,42 @@
+package com.example.gateway.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // Default cache configuration
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(3600)) // Default TTL for all caches
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<>(Object.class)));
+
+        // Custom configurations for different entities
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        // Cache configuration for Tokens
+        cacheConfigurations.put("token",
+                defaultConfig.serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<>(String.class))));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultConfig) // Default settings
+                .withInitialCacheConfigurations(cacheConfigurations) // Custom per-cache configurations
+                .build();
+    }
+
+}
--- gateway/src/main/java/com/example/gateway/services/AuthService.java
+++ gateway/src/main/java/com/example/gateway/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.gateway.services;
 
 import com.example.gateway.clients.AuthClient;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -10,10 +11,13 @@ import java.util.Map;
 
 @Service
 public class AuthService {
+
     AuthClient authClient;
     public AuthService(AuthClient authClient) {
         this.authClient = authClient;
     }
+
+    @Cacheable(value = "token", key = "#token")
     public Mono<ResponseEntity<Map<String, Object>>> validateToken(String token) {
         return Mono.fromCallable(() -> authClient.validateToken(token))
                 .subscribeOn(Schedulers.boundedElastic()); // Offload blocking
