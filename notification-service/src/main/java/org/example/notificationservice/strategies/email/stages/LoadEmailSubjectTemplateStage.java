411bc923966244606d9541940889de5d8d50184b
Mathew Hany <mathew.hanybb@gmail.com>
2025-05-01 22:05:43 +0300

    feat: load email subject template

--- notification-service/src/main/java/org/example/notificationservice/strategies/email/stages/LoadEmailSubjectTemplateStage.java
+++ notification-service/src/main/java/org/example/notificationservice/strategies/email/stages/LoadEmailSubjectTemplateStage.java
@@ -1,15 +1,34 @@
 package org.example.notificationservice.strategies.email.stages;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.notificationservice.config.EmailSubjectTemplatesConfig;
 import org.example.notificationservice.models.Notification;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class LoadEmailSubjectTemplateStage extends EmailNotificationChain {
+    private final EmailSubjectTemplatesConfig templatesConfig;
+
     @Override
     public boolean executeStage(Notification notification, Context context) {
         log.info("Load email subject template...");
+
+        String template =
+            templatesConfig.getSubjectTemplateForNotificationType(notification.getType());
+
+        if (template == null) {
+            log.error(
+                "No subject template found for notification type: {}",
+                notification.getType()
+            );
+            return false;
+        }
+
+        context.setSubjectTemplate(template);
+
         return true;
     }
 }
