dc6fe1cdf54ed3dd9ad27575229f622ae1e1217a
Mathew Hany <mathew.hanybb@gmail.com>
2025-05-01 19:51:34 +0300

    feat: add notification settings

--- notification-service/src/main/java/org/example/notificationservice/models/NotificationStrategyType.java
+++ notification-service/src/main/java/org/example/notificationservice/models/NotificationStrategyType.java
@@ -1,6 +1,19 @@
 package org.example.notificationservice.models;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class NotificationStrategyType {
     public static final String INBOX = "inbox"; // Adds the notification to the database
     public static final String EMAIL = "email";
+    
+    /**
+     * Returns a list of all notification strategy types
+     */
+    public static List<String> getAllTypes() {
+        return Arrays.asList(
+            INBOX,
+            EMAIL
+        );
+    }
 }
