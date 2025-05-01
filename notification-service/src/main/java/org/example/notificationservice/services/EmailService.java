7a89f341b1ffdd8261b98e485fd77bb5ce12b770
Mathew Hany <mathew.hanybb@gmail.com>
2025-05-01 22:08:05 +0300

    feat: email chain leftovers

--- notification-service/src/main/java/org/example/notificationservice/services/EmailService.java
+++ notification-service/src/main/java/org/example/notificationservice/services/EmailService.java
@@ -1,5 +1,7 @@
 package org.example.notificationservice.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,13 +20,19 @@ public class EmailService {
     private JavaMailSender mailSender;
 
     public void sendEmail(Email email) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email.getToEmail());
-        message.setSubject(email.getSubject());
-        message.setText(email.getBody());
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        mailSender.send(message);
-        log.info("Email sent to {}", email.getToEmail());
+            helper.setTo(email.getToEmail());
+            helper.setSubject(email.getSubject());
+            helper.setText(email.getBody(), true);
+
+            mailSender.send(message);
+            log.info("Email sent to {}", email.getToEmail());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Data
