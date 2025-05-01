1b5c3a9a322e71ddba22219d08ecd6213535b268
Darin Mohamed <103316419+Dareenfadel@users.noreply.github.com>
2025-05-01 13:34:16 +0300

    add webflux dependency

--- gateway/src/main/java/com/example/gateway/config/FeignConfig.java
+++ gateway/src/main/java/com/example/gateway/config/FeignConfig.java
@@ -3,6 +3,7 @@ package com.example.gateway.config;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
@@ -14,4 +15,5 @@ public class FeignConfig {
         HttpMessageConverter<?> additional = new MappingJackson2HttpMessageConverter();
         return new HttpMessageConverters(additional);
     }
+
 }
