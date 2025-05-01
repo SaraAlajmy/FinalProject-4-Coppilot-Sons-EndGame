1a783801f9997da5d219620c0b16213c5fbe4c55
Youssof-Kandil <yaskm2002@gmail.com>
2025-05-01 20:26:12 +0300

    finished the service that sends notifications.

--- notification-service/src/main/java/org/example/notificationservice/services/EmailService.java
+++ notification-service/src/main/java/org/example/notificationservice/services/EmailService.java
@@ -4,22 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
     public void sendEmail(Email email) {
-        // TODO: Implement email sending logic
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email.getToEmail());
+        message.setSubject(email.getSubject());
+        message.setText(email.getBody());
+
+        mailSender.send(message);
+        log.info("Email sent to {}", email.getToEmail());
     }
 
     @Data
     @Builder
     @AllArgsConstructor
     public static class Email {
-        private String fromEmail;
         private String toEmail;
         private String subject;
         private String body;
     }
 }
+
