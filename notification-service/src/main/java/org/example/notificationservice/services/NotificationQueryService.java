2705b21b7b7dc0f06bd35dc652806ffe32926c7d
Youssof-Kandil <yaskm2002@gmail.com>
2025-05-02 18:38:30 +0300

    added delete and update to the controller and the service
    # Conflicts:
    #	notification-service/src/main/java/org/example/notificationservice/controllers/NotificationController.java

--- notification-service/src/main/java/org/example/notificationservice/services/NotificationQueryService.java
+++ notification-service/src/main/java/org/example/notificationservice/services/NotificationQueryService.java
@@ -4,10 +4,13 @@ import org.example.notificationservice.models.MessageNotification;
 import org.example.notificationservice.models.Notification;
 import org.example.notificationservice.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,5 +40,23 @@ public class NotificationQueryService {
                     }
                     return "Unknown Sender";
                 }));
+    }
+
+    
+    public Notification updateNotification(String id, Notification updated) {
+        Optional<Notification> existingOpt = notificationRepository.findById(id);
+
+        if (existingOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Notification not found");
+        }
+
+        return notificationRepository.save(updated);
+    }
+
+    public void deleteNotification(String id) {
+        if (!notificationRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Notification not found");
+        }
+        notificationRepository.deleteById(id);
     }
 }
