530e15a1be56d408a38b46a4f69e9d794041bc95
farah alfawzy <farah.alfawzy@gmail.com>
2025-05-11 05:49:14 +0300

    add: deployment files for user-service, postgres, redis

--- userService/src/main/java/com/example/clients/EmailClient.java
+++ userService/src/main/java/com/example/clients/EmailClient.java
@@ -2,13 +2,17 @@ package com.example.clients;
 
 import com.example.models.EmailRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "notification-service", url = "http://notification-service") // or use service discovery
+@FeignClient(name = "notification-service", url = "${notification.service.url}") // or use service discovery
     public interface EmailClient {
 
         @PostMapping("/api/email/send")
         void sendEmail(@RequestBody EmailRequest emailRequest);
     }
+
