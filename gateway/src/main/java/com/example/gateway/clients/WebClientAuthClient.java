05316c8b25b4ddde6c6b46d477294e7663d7d90d
Darin Mohamed <103316419+Dareenfadel@users.noreply.github.com>
2025-05-12 19:33:20 +0300

    change to webclient insted of openfeign

---
+++ gateway/src/main/java/com/example/gateway/clients/WebClientAuthClient.java
@@ -0,0 +1,30 @@
+package com.example.gateway.clients;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import java.util.Map;
+
+@Component
+public class WebClientAuthClient  {
+
+    private final WebClient webClient;
+
+    public WebClientAuthClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("http://localhost:8086")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .build();
+    }
+
+    public Mono<ResponseEntity<Map<String, Object>>> validateToken(String token) {
+        return webClient.post()
+                .uri("/auth/validateToken")
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<Map<String, Object>>() {});
+    }
+}
