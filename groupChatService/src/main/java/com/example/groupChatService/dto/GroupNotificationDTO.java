52b5bc08d9f73da851a056ae2d730ca9685a8320
Shorok02 <shorokabdulraof@gmail.com>
2025-05-15 21:52:06 +0300

    adding rabbit mq to group chat service

--- groupChatService/src/main/java/com/example/groupChatService/dto/GroupNotificationDTO.java
+++ groupChatService/src/main/java/com/example/groupChatService/dto/GroupNotificationDTO.java
@@ -14,7 +14,6 @@ public class GroupNotificationDTO {
     private String recipientUserId;
     private String type; // "group_message" or "group_mention"
     private String senderUserId;
-    private String senderUserName;
     private String messageId;
     private String messageText;
     private LocalDateTime messageTimestamp;
--- groupChatService/src/main/java/com/example/groupChatService/services/NotificationListener.java
+++ groupChatService/src/main/java/com/example/groupChatService/services/NotificationListener.java
@@ -1,35 +1,75 @@
 package com.example.groupChatService.services;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import com.example.groupChatService.dto.GroupNotificationDTO;
+import com.example.groupChatService.models.GroupChat;
 import com.example.groupChatService.models.GroupMessage;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.example.groupChatService.rabbitmq.NotificationProducer;
+import com.example.groupChatService.repositories.GroupChatRepo;
 
 @Component
 public class NotificationListener implements MessageListener {
 
-    private final RabbitTemplate rabbitTemplate;
-    private final ObjectMapper objectMapper;
+    private final GroupChatRepo groupChatRepo;
+    private final NotificationProducer notificationProducer;
 
-    public NotificationListener(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.objectMapper = new ObjectMapper();
-        this.objectMapper.registerModule(new JavaTimeModule());
+    @Autowired
+    public NotificationListener(GroupChatRepo groupChatRepo, NotificationProducer notificationProducer) {
+        this.notificationProducer = notificationProducer;
+        this.groupChatRepo = groupChatRepo;
     }
 
 
     @Override
-    public void onNewMessage(GroupMessage message) {
-        try {
-            String json = objectMapper.writeValueAsString(message);
-            System.out.println("🔔 NotificationListener received new message: " + json);
-            rabbitTemplate.convertAndSend("notificationQueue", json);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
+    public void onNewMessage(GroupMessage message, String senderUsername) {
+        // get all users other than the sender of the message
+        GroupChat group = groupChatRepo.findById(message.getGroupId()).orElseThrow(() -> new RuntimeException("Group not found with id:" + message.getGroupId()));
+
+        List<String> recipientsUserIds = new ArrayList<>();
+        for (String memberId : group.getMembers()) {
+            if (!memberId.equals(message.getSenderId())) {
+                recipientsUserIds.add(memberId);
+            }
         }
+        List<String> mentionedUserIds = message.getMentionedUserIds();
+
+        GroupNotificationDTO notification = new GroupNotificationDTO();
+        notification.setType("group_mention");
+        notification.setSenderUserId(senderUsername);
+        notification.setMessageId(message.getId());
+        notification.setMessageText(message.getContent());
+        notification.setMessageTimestamp(message.getCreatedAt());
+        notification.setGroupId(message.getGroupId());
+        notification.setGroupName(group.getName());
+        notification.setGroupIcon(group.getEmoji());
+
+        for (String recipientUserId : recipientsUserIds) {
+            if (mentionedUserIds.contains(recipientUserId)) {
+                recipientsUserIds.remove(recipientUserId);
+                notification.setRecipientUserId(recipientUserId);
+                // send notification to mentioned users
+                try {
+                    notificationProducer.sendMessage(notification);
+                } catch (Exception e) {
+                    throw new RuntimeException("Error sending notification: " + e.getMessage());
+                }
+            } 
+        }
+
+        notification.setType("group_message");
+        for(String recipientUserId : recipientsUserIds) {
+             notification.setRecipientUserId(recipientUserId);
+            // send notification to other group members
+            try {
+                notificationProducer.sendMessage(notification);
+            } catch (Exception e) {
+                throw new RuntimeException("Error sending notification: " + e.getMessage());
+            }
+        }
+
     }
+
 }
