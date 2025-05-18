9fedab22d471289a35186cfedaba2d94a285c874
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-18 22:10:49 +0300

    Message Polling check user and chatId

--- chat-service/src/main/java/com/example/chat_service/services/chat/ChatService.java
+++ chat-service/src/main/java/com/example/chat_service/services/chat/ChatService.java
@@ -1,6 +1,7 @@
 package com.example.chat_service.services.chat;
 
 import com.example.chat_service.controllers.ChatController;
+import com.example.chat_service.exceptions.UnauthorizedOperationException;
 import com.example.chat_service.models.Chat;
 import com.example.chat_service.models.Message;
 import com.example.chat_service.repositories.ChatRepository;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.Optional;
@@ -52,11 +54,16 @@ public class ChatService {
         return chatRepository.findByParticipantOneIdOrParticipantTwoId(userId, userId);
     }
 
-    public List<Message> getLatestMessages(String chatId, String lastMessageId) {
+    public List<Message> getLatestMessages(String userId, String chatId, String lastMessageId) {
         Optional<Message> lastMessage = messageRepository.findById(lastMessageId);
         if (lastMessage.isEmpty()) {
             logger.error("Last message with ID {} not found in chat {}", lastMessageId, chatId);
             throw new RuntimeException("Last message not found");
+        }
+        Chat chat = getChatById(chatId);
+        if (!chat.getParticipantOneId().equals(userId) && !chat.getParticipantTwoId().equals(userId)) {
+            logger.error("User {} is not a participant in chat {}", userId, chatId);
+            throw new UnauthorizedOperationException("User is not a participant in this chat");
         }
         logger.info("Retrieving messages for chat {} after message {}", chatId, lastMessageId);
         return messageRepository.findByChatIdAndCreatedAtAfter(chatId, lastMessage.get().getCreatedAt());
