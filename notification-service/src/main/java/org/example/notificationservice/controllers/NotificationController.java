a73a055281056d0216f5851b90cf22553ef0a339
Mathew Hany <mathew.hanybb@gmail.com>
2025-05-18 19:12:53 +0300

    lint: format

--- notification-service/src/main/java/org/example/notificationservice/controllers/NotificationController.java
+++ notification-service/src/main/java/org/example/notificationservice/controllers/NotificationController.java
@@ -15,34 +15,50 @@ import java.util.Map;
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
-private NotificationQueryService notificationQueryService;
-private NotificationService notificationService;
-@Autowired
-public NotificationController(NotificationQueryService notificationQueryService,
-                              NotificationService notificationService) {
-    this.notificationQueryService = notificationQueryService;
-    this.notificationService = notificationService;
-}
+    private NotificationQueryService notificationQueryService;
+    private NotificationService notificationService;
 
-@GetMapping("/unread")
-public ResponseEntity<List<Notification>> getAllUnreadNotifications(@RequestHeader String userId) {
-    List<Notification> notifications = notificationQueryService.getAllUnreadNotifications(userId);
-    return ResponseEntity.ok(notifications);
-}
-@GetMapping("/unread/grouped")
-public ResponseEntity<Map<String, List<Notification>>> getAllUnreadNotificationsGroupedBySender(@RequestHeader String userId) {
-    Map<String, List<Notification>> notifications = notificationQueryService.getAllUnreadNotificationsGroupedBySender(userId);
-    return ResponseEntity.ok(notifications);
-}
-@PostMapping("/reset-password")
-public ResponseEntity<String> sendResetPasswordNotification(@RequestParam String resetLink,
-                                                            @RequestParam String recipientEmail,
-                                                            @RequestParam String recipientName) {
-     if(notificationService.sendResetPasswordNotification(resetLink, recipientEmail,recipientName))
-        return ResponseEntity.ok("Reset password notification sent successfully.");
-    else
-        return ResponseEntity.status(500).body("Failed to send reset password notification.");
-}
+    @Autowired
+    public NotificationController(
+        NotificationQueryService notificationQueryService,
+        NotificationService notificationService
+    ) {
+        this.notificationQueryService = notificationQueryService;
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/unread")
+    public ResponseEntity<List<Notification>> getAllUnreadNotifications(@RequestHeader String userId) {
+        List<Notification> notifications =
+            notificationQueryService.getAllUnreadNotifications(userId);
+        return ResponseEntity.ok(notifications);
+    }
+
+    @GetMapping("/unread/grouped")
+    public ResponseEntity<Map<String, List<Notification>>> getAllUnreadNotificationsGroupedBySender(
+        @RequestHeader String userId
+    ) {
+        Map<String, List<Notification>> notifications =
+            notificationQueryService.getAllUnreadNotificationsGroupedBySender(userId);
+        return ResponseEntity.ok(notifications);
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> sendResetPasswordNotification(
+        @RequestParam String resetLink,
+        @RequestParam String recipientEmail,
+        @RequestParam String recipientName
+    ) {
+        if (notificationService.sendResetPasswordNotification(
+            resetLink,
+            recipientEmail,
+            recipientName
+        )) {
+            return ResponseEntity.ok("Reset password notification sent successfully.");
+        } else {
+            return ResponseEntity.status(500).body("Failed to send reset password notification.");
+        }
+    }
 
 
     @GetMapping("/AllNotifications")
@@ -65,20 +81,26 @@ public ResponseEntity<String> sendResetPasswordNotification(@RequestParam String
 
     @PostMapping("/markAllRead")
     public ResponseEntity<String> markAllNotificationsAsRead(@RequestHeader String userId) {
-       return ResponseEntity.ok(notificationQueryService.markAllNotificationsAsRead(userId));
+        return ResponseEntity.ok(notificationQueryService.markAllNotificationsAsRead(userId));
     }
+
     @DeleteMapping()
-    public ResponseEntity<String> deleteNotification(@RequestHeader String userId, @RequestParam String notificationId) {
+    public ResponseEntity<String> deleteNotification(
+        @RequestHeader String userId,
+        @RequestParam String notificationId
+    ) {
         notificationQueryService.deleteNotification(notificationId, userId);
         return ResponseEntity.status(200).body("Notification deleted successfully");
     }
-    @PutMapping()
-    public ResponseEntity<Notification> updateNotification(@RequestParam String notificationId,
-                                                     @RequestBody Notification newNotification) {
 
-        Notification updatedNotfication=notificationQueryService.updateNotification(notificationId, newNotification);
+    @PutMapping()
+    public ResponseEntity<Notification> updateNotification(
+        @RequestParam String notificationId,
+        @RequestBody Notification newNotification
+    ) {
+
+        Notification updatedNotfication =
+            notificationQueryService.updateNotification(notificationId, newNotification);
         return ResponseEntity.status(200).body(updatedNotfication);
     }
-
-
 }
